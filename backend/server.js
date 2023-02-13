ec7abc657d994a09ab5aedcdd6e803221c7071f4
Rijul Sarji <64856177+rijulsarji@users.noreply.github.com>
2023-02-13 22:34:42 +0530

    connected database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const app =require("./app");
 const dotenv = require("dotenv");
 
-dotenv.config({ path: "backend/config/config.env" })
+dotenv.config({ path: "backend/config/config.env" });
 
+// always implement config functions after dotenv.config
 const PORT = process.env.PORT;
+const connectDatabase = require("./config/database");
 
+// initialising server
 app.listen(PORT, () => {
   console.log(`Listening to ${PORT}...`)
 })
+
+// connecting to database
+connectDatabase();
